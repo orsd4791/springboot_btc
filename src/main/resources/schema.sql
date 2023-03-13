f5a1f7e875703258ffb5bac91cf75ee5b9259273
hyunyoung.park <hyunyoung.park@bespinglobal.com>
2023-03-13 15:53:06 +0900

    index.html

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,4 +14,4 @@ create table eo.sample (
                            upd_dt datetime not null comment 'update date'
 ) comment 'sample';
 create index eo_sample_x01 on eo.sample (sample_tit);
-/* test */
+/* testetwsetetsetsetsetset */
